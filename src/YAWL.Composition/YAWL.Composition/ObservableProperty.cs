3fb0b912fae42b1f067c162f7e9fe273934b3a6d
Toni Petrina <tpetrina@tpetrina.com>
2015-09-03 22:32:17 +0200

    Added ObservableProperty<T>

---
+++ src/YAWL.Composition/YAWL.Composition/ObservableProperty.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace YAWL.Composition
+{
+    public class ObservableProperty<T> : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        protected static PropertyChangingEventArgs ValueChanging => new PropertyChangingEventArgs(nameof(Value));
+        protected static PropertyChangedEventArgs ValueChanged => new PropertyChangedEventArgs(nameof(Value));
+
+        private T value;
+        public T Value
+        {
+            get { return value; }
+            set
+            {
+                if (!EqualityComparer<T>.Default.Equals(value, this.value))
+                {
+                    OnPropertyChanging(ValueChanging);
+                    this.value = value;
+                    OnPropertyChanged(ValueChanged);
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChanged?.Invoke(this, eventArgs);
+
+        }
+        #endregion
+
+        #region INotifyPropertyChanging implementation
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected virtual void OnPropertyChanging(PropertyChangingEventArgs eventArgs)
+        {
+            PropertyChanging?.Invoke(this, eventArgs);
+
+        }
+        #endregion
+
+        public ObservableProperty(T value = default(T))
+        {
+            this.value = value;
+        }
+
+        public static implicit operator T(ObservableProperty<T> property)
+        {
+            return property.value;
+        }
+    }
+}
