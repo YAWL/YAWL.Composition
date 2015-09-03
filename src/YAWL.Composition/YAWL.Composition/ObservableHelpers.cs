c1bd7cf7afe9c22f3e7487d45cc4136d9e1edf9c
Toni Petrina <tpetrina@tpetrina.com>
2015-09-03 23:05:02 +0200

    Applied license and added helpers for creating derived observables

---
+++ src/YAWL.Composition/YAWL.Composition/ObservableHelpers.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+
+using System;
+using System.ComponentModel;
+
+namespace YAWL.Composition
+{
+    public static class ObservableHelpers
+    {
+        public static ObservableProperty<TResult> Map<T, TResult>(this ObservableProperty<T> property,
+            Func<T, TResult> map)
+        {
+            if (property == null)
+                throw new ArgumentNullException(nameof(property));
+            if (map == null)
+                throw new ArgumentNullException(nameof(map));
+
+            var result = new ObservableProperty<TResult>(map(property.Value));
+
+            property.PropertyChanged += (sender, args) =>
+            {
+                result.Value = map(property.Value);
+            };
+
+            return result;
+        }
+
+        public static ObservableProperty<TResult> Combine<T1, T2, TResult>(this ObservableProperty<T1> property1,
+            ObservableProperty<T2> property2, Func<T1, T2, TResult> combine)
+        {
+            var result = new ObservableProperty<TResult>(combine(property1.Value, property2.Value));
+
+            PropertyChangedEventHandler handler = (sender, args) =>
+            {
+                result.Value = combine(property1.Value, property2.Value);
+            };
+
+            property1.PropertyChanged += handler;
+            property2.PropertyChanged += handler;
+
+            return result;
+        }
+    }
+}
--- src/YAWL.Composition/YAWL.Composition/ObservableProperty.cs
+++ src/YAWL.Composition/YAWL.Composition/ObservableProperty.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace YAWL.Composition
