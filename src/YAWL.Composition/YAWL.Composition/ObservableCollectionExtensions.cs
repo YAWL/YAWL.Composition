7a0367f571da8403d0a567680bebdbace0bb7d3f
Toni Petrina <tpetrina@tpetrina.com>
2015-09-28 22:11:49 +0200

    Added Reduce extension method

---
+++ src/YAWL.Composition/YAWL.Composition/ObservableCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace YAWL.Composition
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static ObservableProperty<TReturn> Reduce<TInput, TReturn>(
+            this ObservableCollection<TInput> collection,
+            Func<IEnumerable<TInput>, TReturn> reducer)
+        {
+            if (collection == null)
+                return new ObservableProperty<TReturn>();
+
+            var initialValue = reducer == null ? default(TReturn) : reducer(collection);
+
+            var prop = new ObservableProperty<TReturn>(initialValue);
+
+            NotifyCollectionChangedEventHandler handler;
+            handler = (sender, args) =>
+            {
+                prop.Value = reducer != null ? reducer(collection) : default(TReturn);
+            };
+            collection.CollectionChanged += handler;
+
+            return prop;
+        }
+    }
+}
